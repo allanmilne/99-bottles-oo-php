043bbaa7c10a33592316d457fa605b9ba9932fff
allanmilne <amilne.wd@gmail.com>
2021-07-26 07:29:51 +0100

    Add BottleNumber factory

--- src/Bottles.php
+++ src/Bottles.php
@@ -3,6 +3,7 @@ declare(strict_types=1);
 
 use JetBrains\PhpStorm\Pure;
 require __DIR__ . "/../src/BottleNumber.php";
+require __DIR__ . "/../src/BottleNumber0.php";
 
 final class Bottles
 {
@@ -29,13 +30,24 @@ final class Bottles
 
     #[Pure] public function verse(int $number): string
     {
-        $bottleNumber = new BottleNumber($number);
-        $nextBottleNumber = new BottleNumber($bottleNumber->successor());
+        $bottleNumber = $this->bottleNumberFor($number);
+        $nextBottleNumber = $this->bottleNumberFor($bottleNumber->successor());
 
         return
             ucfirst("{$bottleNumber} of beer on the wall, ") .
             "{$bottleNumber} of beer.\n" .
             $bottleNumber->action() .
             "{$nextBottleNumber} of beer on the wall.\n";
+    }
+
+    public function bottleNumberFor(int $number): BottleNumber
+    {
+        if ($number === 0) {
+            $className = BottleNumber0::class;
+        } else {
+            $className = BottleNumber::class;
+        }
+
+        return new $className($number);
     }
 }
