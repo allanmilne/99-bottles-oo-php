7653e4d39c4fedcd3dccc46f611ebcacef3316ed
allanmilne <amilne.wd@gmail.com>
2021-08-11 07:14:03 +0100

    Move lyrics method into BottleVerse abstraction

--- src/BottleVerse.php
+++ src/BottleVerse.php
@@ -1,6 +1,25 @@
 <?php
 
-class BottlesVerse
+class BottleVerse
 {
-    
+    private $number;
+
+    public function __construct($number)
+    {
+        $this->number = $number;
+    }
+
+    /**
+     * @return string
+     */
+    public function lyrics(): string
+    {
+        $bottleNumber = BottleNumber::for($this->number);
+
+        return
+            ucfirst("$bottleNumber of beer on the wall, ") .
+            "$bottleNumber of beer.\n" .
+            $bottleNumber->action() .
+            "{$bottleNumber->successor()} of beer on the wall.\n";
+    }
 }
