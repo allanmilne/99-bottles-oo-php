c93b3a0b553b98484739b849391b74e12c2469c7
allanmilne <amilne.wd@gmail.com>
2021-08-21 07:45:09 +0100

    Refactor BottleVerse class making it injectable to Bottles

--- src/BottleVerse.php
+++ src/BottleVerse.php
@@ -1,25 +1,33 @@
 <?php
+declare(strict_types=1);
 
 class BottleVerse
 {
-    private $number;
+    protected object $bottleNumber;
 
-    public function __construct($number)
+    public function __construct(object $number)
     {
-        $this->number = $number;
+        $this->bottleNumber = $number;
+    }
+
+    /**
+     * @param int $number
+     * @return string
+     */
+    public static function lyrics(int $number): string
+    {
+        return (new BottleVerse(BottleNumber::for($number)))->_lyrics();
     }
 
     /**
      * @return string
      */
-    public function lyrics(): string
+    public function _lyrics(): string
     {
-        $bottleNumber = BottleNumber::for($this->number);
-
         return
-            ucfirst("$bottleNumber of beer on the wall, ") .
-            "$bottleNumber of beer.\n" .
-            $bottleNumber->action() .
-            "{$bottleNumber->successor()} of beer on the wall.\n";
+            ucfirst("$this->bottleNumber of beer on the wall, ") .
+            "$this->bottleNumber of beer.\n" .
+            $this->bottleNumber->action() .
+            "{$this->bottleNumber->successor()} of beer on the wall.\n";
     }
 }
